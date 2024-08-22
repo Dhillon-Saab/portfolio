09f79b6e626836edfa44c466aeb30e97969dbe7a
Dhillon-Saab <ad70138@gmail.com>
2024-08-22 02:38:23 -0600

    Added Footer

---
+++ app/_componenets/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='fixed bottom-0'>
+    <div className="flex h-0.5 bg-white mt-3"></div>
+    <p className=' bg-black w-screen m-auto align-center text-center text-bold justify-center p-4'>Copyright © Amrit Singh Dhillon.</p>
+    <p className=' bg-black w-screen m-auto align-center text-center text-bold justify-center'>Get in Contact <a href='https://github.com/Dhillon-Saab'>Github</a></p>
+    </div>
+  )
+}   
+
+export default Footer
