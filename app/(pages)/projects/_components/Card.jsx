d014f50c0674c48ed7fc2ff9a16cfa40e7122a65
Dhillon-Saab <ad70138@gmail.com>
2024-08-23 04:12:42 -0600

    changes to the front-end as well as some backend

--- app/(pages)/projects/_components/Card.jsx
+++ app/(pages)/projects/_components/Card.jsx
@@ -2,14 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Card({link, heading, subheading }) {
+function Card({ link, heading, subheading, tags }) {
   return (
     <Link href={link}>
       <div className="flex flex-col gap-10 align-middle items-center justify-center mt-10">
-        <div className="flex flex-col pt-2 border-2 rounded-xl w-[49rem] h-auto pb-4 items-center text-start justify-left pl-4 text-3xl">
-          <Image width={200} height={200} src={"/shift-sync.jpg"} alt="shift sync" className="rounded-full my-4" />
+        <div className="flex flex-col pt-2 border-2 rounded-xl w-[49rem] h-auto pb-4 text-start items-center justify-center pl-4 text-3xl">
+          <Image
+            width={200}
+            height={200}
+            src={"/shift-sync.jpg"}
+            alt="shift sync"
+            className="rounded-full my-4"
+          />
           {heading}
-          <div className="flex text-sm mt-1 p-4">{subheading?.split(" ").splice(0,25).join(" ") }...</div>
+          <div className="flex text-sm mt-1 p-4">
+            {subheading?.split(" ").splice(0, 25).join(" ")}...
+          </div>
+          <div className="flex gap-3 justify-start align-top items-start mr-[28rem]">
+            {tags?.map((tag) => {
+              return (
+                <div className="text-sm bg-gray-800 p-2 rounded-full">{tag}</div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Link>
--- app/(pages)/projects/page.jsx
+++ app/(pages)/projects/page.jsx
@@ -11,6 +11,7 @@ function page() {
         subheading={
           "Shift Sync is an intelligent and dynamic employee scheduling solution designed to simplify the complex task of workforce management. By leveraging cutting-edge technologies like Next.js, Firebase, and OR-Tools, Shift Sync automates schedule generation based on employee preferences while allowing managers to make final adjustments with ease. Whether you need to accommodate full-time and part-time staff or prioritize supervisors, Shift Sync has you covered."
         }
+        tags={["React", "Nextjs", "Firebase", "Authjs"]}
       />
     </div>
   );
