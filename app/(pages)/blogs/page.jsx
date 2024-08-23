86e5ecc2bf8cf58a268b5c465a47303f1a4b639f
Dhillon-Saab <ad70138@gmail.com>
2024-08-23 02:43:23 -0600

    Made blogs page dynamic

--- app/(pages)/blogs/page.jsx
+++ app/(pages)/blogs/page.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Card from "./_components/Card";
+import prisma from "@/lib/prisma";
 
-function page() {
+const getBlogs = async ()=>{
+  const blogs = await prisma.blog.findMany({
+    where: {published : true},
+  })
+  return blogs;
+}
+
+async function page() {
+  
+  const blogs = await getBlogs();
   return (
     <div>
-      <Card link={"/blogs/123"} heading={"First Blog Post"} subheading={"This is the starting post for my blogging webpage"} />
+      {blogs.map((blog)=>{
+        return(
+          <Card key={blog.id} link={`/blogs/${blog.id}`} heading={blog.title} subheading={blog.content} />
+        )
+      })}
+      {/* <Card link={"/blogs/123"} heading={"First Blog Post"} subheading={"This is the starting post for my blogging webpage"} /> */}
     </div>
   );
 }
